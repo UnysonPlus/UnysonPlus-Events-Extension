83ef30db9d9891e16ca1cdc4c348547baf9ed3be
jonmlas <jonmlas@gmail.com>
2025-08-21 18:54:23 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,17 +1,29 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
+<?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array();
 
 $manifest['name']        = __( 'Events', 'fw' );
-$manifest['description'] = __( 'This extension adds a fully fledged Events module to your theme. It comes with built in pages that contain a calendar where events can be added.', 'fw' );
-$manifest['version'] = '1.0.15';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Events-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/events/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+$manifest['slug']        = 'unysonplus-events';
+$manifest['description'] = __( 
+	'This extension adds a fully fledged Events module to your theme. It comes with built-in pages that contain a calendar where events can be added.', 
+	'fw' 
+);
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Events-Extension';
+$manifest['version']     = '1.0.15';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Events-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Events-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
